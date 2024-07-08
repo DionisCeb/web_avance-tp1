704b9f66f24b5e6ae05526328efc0414bfa003b6
Dionis <e2396498@cmaisonneuve.qc.ca>
2024-07-08 02:01:43 -0400

    adding the interface

---
+++ assets/js/scrolling.js
@@ -0,0 +1,14 @@
+/* function smoothScroll(event) {
+    event.preventDefault(); 
+    const targetId = event.target.getAttribute('href').substring(1); 
+    const targetSection = document.getElementById(targetId); 
+    targetSection.scrollIntoView({ behavior: 'smooth' }); 
+}
+
+document.getElementById('scrollToGroupImage').addEventListener('click', function() {
+    document.getElementById('reserve-sec').scrollIntoView({
+        behavior: 'smooth'
+    });
+});
+
+ */
